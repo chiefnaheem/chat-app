9cbabbbe9ac3494a0ae5823d56a2ba1a6f10fdca
chiefnaheem <naheemademola21@gmail.com>
2023-01-16 11:56:05 -0800

    auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { envConfig } from 'src/config/constant/env.configuration';
+
 import { configValidation } from 'src/config/validator/config.validator';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>(envConfig.CONNECTION_STRING),
-      }),
-      inject: [ConfigService],
-    }),
+   
     ConfigModule.forRoot({
       isGlobal: true,
       cache: false,
