55a88e254c21c1acf20729a97395150832f44e0a
chiefnaheem <naheemademola21@gmail.com>
2023-01-18 06:11:03 -0800

    connect to db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { configValidation } from 'src/config/validator/config.validator';
 import { AppController } from './app.controller';
@@ -7,7 +8,17 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
-   
+    TypeOrmModule.forRoot({
+      keepConnectionAlive: true,
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT || 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       cache: false,
