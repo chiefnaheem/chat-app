e17daa21f187c6766a32e5ab64d74b2a93e7d748
chiefnaheem <naheemademola21@gmail.com>
2023-01-18 07:19:30 -0800

    commit updates

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.use(session({
+    secret: process.env.COOKIES_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 86400000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+    }
+  }))
+  app.use(passport.initialize());
+  app.use(passport.session());
   await app.listen(3006);
 }
 bootstrap();
